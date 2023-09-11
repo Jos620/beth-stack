7ba1c86bf2c8a6c62896a1725eb241c9bd81beaf
Mateus Ito <mateusito10@gmail.com>
2023-09-11 14:58:28 -0300

    style(components): align TodoItem action buttons to the right

--- src/components/Todo/Item/index.tsx
+++ src/components/Todo/Item/index.tsx
@@ -6,23 +6,26 @@ export function TodoItem({ content, completed, id }: Todo) {
   const containerId = `todo-${id}`;
 
   return (
-    <div id={containerId} class="flex flex-row space-x-3">
+    <div id={containerId} class="flex flex-row justify-between space-x-3">
       <p>{content}</p>
-      <input
-        type="checkbox"
-        checked={completed}
-        hx-post={`/todos/toggle/${id}`}
-        hx-target={`#${containerId}`}
-        hx-swap="outerHTML"
-      />
-      <button
-        class="text-red-500"
-        hx-delete={`/todos/${id}`}
-        hx-target={`#${containerId}`}
-        hx-swap="outerHTML"
-      >
-        X
-      </button>
+
+      <div>
+        <input
+          type="checkbox"
+          checked={completed}
+          hx-post={`/todos/toggle/${id}`}
+          hx-target={`#${containerId}`}
+          hx-swap="outerHTML"
+        />
+        <button
+          class="text-red-500"
+          hx-delete={`/todos/${id}`}
+          hx-target={`#${containerId}`}
+          hx-swap="outerHTML"
+        >
+          X
+        </button>
+      </div>
     </div>
   );
 }
