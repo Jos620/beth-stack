743c60b4cf3c74b373e6ea159ff0ed3910f5b52d
Mateus Ito <mateusito10@gmail.com>
2023-09-11 18:34:52 -0300

    refactor(ui): move components and layouts to ui folder

--- src/infra/http/controllers/todos.controller.tsx
+++ src/infra/http/controllers/todos.controller.tsx
@@ -4,8 +4,8 @@ import elements from 'typed-html';
 
 import { db } from '@/infra/database';
 import { todos } from '@/infra/database/schema';
+import { TodoItem } from '@/ui/components/Todo/Item';
 
-import { TodoItem } from '../../../components/Todo/Item';
 import { CreateTodoDto, UpdateTodoDto } from '../dto/todos';
 
 export const todosController = new Elysia({ prefix: '/todos' })
