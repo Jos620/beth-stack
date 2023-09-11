f1ef318d5d09e329f5801a1c99d0bb3f78a2eb2c
Mateus Ito <mateusito10@gmail.com>
2023-09-11 14:43:04 -0300

    feat(components): clean TodoForm on submit

--- src/components/Todo/Form/index.tsx
+++ src/components/Todo/Form/index.tsx
@@ -6,6 +6,7 @@ export function TodoForm() {
       class="flex flex-row space-x-3"
       hx-post="/todos"
       hx-swap="beforebegin"
+      _="on submit target.reset()"
     >
       <input type="text" name="content" class="border border-black" />
       <button type="submit">Add</button>
