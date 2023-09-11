178440295f23d3c8e957de2ef669f6dbcb11323c
Mateus Ito <mateusito10@gmail.com>
2023-09-11 09:41:59 -0300

    feat(package): setup elysia

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-console.log("Hello via Bun!");
+import { Elysia } from 'elysia'
+
+const app = new Elysia().get('/', () => 'Hello, World!').listen(3000)
+console.log(
+  `Server running at http://${app.server?.hostname}:${app.server?.port}`
+)
