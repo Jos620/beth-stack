25794f56b8e635dee02704b35735cbf443e30a14
Mateus Ito <mateusito10@gmail.com>
2023-09-11 10:50:23 -0300

    feat(components): toggle todo on checkbox click

--- src/components/Todo/Item/index.tsx
+++ src/components/Todo/Item/index.tsx
@@ -2,11 +2,16 @@ import * as elements from 'typed-html';
 
 import { Todo } from '../../../entities/todo';
 
-export function TodoItem({ content, completed }: Todo) {
+export function TodoItem({ content, completed, id }: Todo) {
   return (
-    <div class="flex flex-row space-x-3">
+    <div id={`todo-${id}`} class="flex flex-row space-x-3">
       <p>{content}</p>
-      <input type="checkbox" checked={completed} />
+      <input
+        type="checkbox"
+        checked={completed}
+        hx-post={`/todos/toggle/${id}`}
+        hx-target={`#todo-${id}`}
+      />
       <button class="text-red-500">X</button>
     </div>
   );
