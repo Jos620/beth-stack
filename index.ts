3d1366b73f7cc01f402f9e49d6e5e5f05dc95a94
Mateus Ito <mateusito10@gmail.com>
2023-09-11 09:46:17 -0300

    feat: setup baseHtml

--- index.ts
+++ index.ts
@@ -1,6 +1,21 @@
 import { Elysia } from 'elysia'
+import { html } from '@elysiajs/html'
 
-const app = new Elysia().get('/', () => 'Hello, World!').listen(3000)
+const app = new Elysia().use(html()).get('/', ({ html }) => html(baseHtml)).listen(3000)
 console.log(
   `Server running at http://${app.server?.hostname}:${app.server?.port}`
 )
+
+const baseHtml = `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BETH stack</title>
+  </head>
+  <body>
+    <h1>Hey there!</h1>
+  </body>
+  </html>
+`
