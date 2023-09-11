985dd80462c5f239261031bdcd2707a88c0d37ea
Mateus Ito <mateusito10@gmail.com>
2023-09-11 10:10:15 -0300

    feat: add JSX support

--- index.tsx
+++ index.tsx
@@ -1,24 +1,33 @@
 import { html } from '@elysiajs/html';
 import { Elysia } from 'elysia';
+import * as elements from 'typed-html';
 
 const app = new Elysia()
   .use(html())
-  .get('/', ({ html }) => html(baseHtml))
+  .get('/', ({ html }) =>
+    html(
+      <BaseHtml>
+        <body>
+          <h1>Hey</h1>
+        </body>
+      </BaseHtml>,
+    ),
+  )
   .listen(3000);
 console.log(
   `Server running at http://${app.server?.hostname}:${app.server?.port}`,
 );
 
-const baseHtml = `
+const BaseHtml = ({ children }: elements.Children) => `
   <!DOCTYPE html>
   <html lang="en">
+
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BETH stack</title>
   </head>
-  <body>
-    <h1>Hey there!</h1>
-  </body>
+
+  ${children}
   </html>
 `;
