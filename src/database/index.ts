4b149756b37ec552e5890668b436d634b1e9424d
Mateus Ito <mateusito10@gmail.com>
2023-09-11 10:49:02 -0300

    fix(database): remove duplicated IDs

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,6 +3,6 @@ import { Todo } from '../entities/todo';
 export const db: Todo[] = [
   { id: 1, content: 'Learn Bun', completed: true },
   { id: 2, content: 'Learn Elysia', completed: false },
-  { id: 2, content: 'Learn Turso', completed: false },
-  { id: 2, content: 'Learn HTMX', completed: false },
+  { id: 3, content: 'Learn Turso', completed: false },
+  { id: 4, content: 'Learn HTMX', completed: false },
 ];
