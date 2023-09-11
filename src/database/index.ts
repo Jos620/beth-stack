4722947c211cec9528ae2e49657c8f328b257e38
Mateus Ito <mateusito10@gmail.com>
2023-09-11 10:31:33 -0300

    feat(database): add mock database

---
+++ src/database/index.ts
@@ -0,0 +1,8 @@
+import { Todo } from '../entities/todo';
+
+export const db: Todo[] = [
+  { id: 1, content: 'Learn Bun', completed: true },
+  { id: 2, content: 'Learn Elysia', completed: false },
+  { id: 2, content: 'Learn Turso', completed: false },
+  { id: 2, content: 'Learn HTMX', completed: false },
+];
