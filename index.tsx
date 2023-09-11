d652fb16ee24c1f127ce50897fc55694269b5b1f
Mateus Ito <mateusito10@gmail.com>
2023-09-11 10:26:49 -0300

    feat: add unocss for styling

--- index.tsx
+++ index.tsx
@@ -7,15 +7,19 @@ const app = new Elysia()
   .get('/', ({ html }) =>
     html(
       <BaseHtml>
-        <body>
-          <button hx-post="/click" hx-swap="outerHTML">
+        <body class="flex justify-center items-center w-screen h-screen">
+          <button
+            hx-post="/click"
+            hx-swap="outerHTML"
+            class="bg-blue-500 rounded text-white p-2"
+          >
             Click me
           </button>
         </body>
       </BaseHtml>,
     ),
   )
-  .post('/click', () => <div>Clicked!</div>)
+  .post('/click', () => <p class="text-dark-400">Clicked!</p>)
   .listen(3000);
 console.log(
   `Server running at http://${app.server?.hostname}:${app.server?.port}`,
@@ -30,6 +34,8 @@ const BaseHtml = ({ children }: elements.Children) => `
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BETH stack</title>
     <script src="https://unpkg.com/htmx.org@1.9.3"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@unocss/runtime"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@unocss/reset/tailwind.css"></script>
   </head>
 
   ${children}
