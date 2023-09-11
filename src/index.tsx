fa101329e57d3812dbece1fae7c170be2f62a6ac
Mateus Ito <mateusito10@gmail.com>
2023-09-11 18:36:56 -0300

    refactor(layouts): move BaseHtml to DefaultBaseHtml

--- src/index.tsx
+++ src/index.tsx
@@ -24,21 +24,3 @@ const app = new Elysia()
 console.log(
   `Server running at http://${app.server?.hostname}:${app.server?.port}`,
 );
-
-export const BaseHtml = ({ children }: elements.Children) => `
-  <!DOCTYPE html>
-  <html lang="en">
-
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BETH stack</title>
-    <script src="https://unpkg.com/htmx.org@1.9.3"></script>
-    <script src="https://unpkg.com/hyperscript.org@0.9.9"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@unocss/runtime"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@unocss/reset/tailwind.css"></script>
-  </head>
-
-  ${children}
-  </html>
-`;
