582c657ade33a2a3e7677729aa4ec1ebe3fd9997
Mateus Ito <mateusito10@gmail.com>
2023-09-11 13:47:00 -0300

    refactor(components): use a variable for the container id

--- src/components/Todo/Item/index.tsx
+++ src/components/Todo/Item/index.tsx
@@ -3,20 +3,22 @@ import elements from 'typed-html';
 import { Todo } from '../../../entities/todo';
 
 export function TodoItem({ content, completed, id }: Todo) {
+  const containerId = `todo-${id}`;
+
   return (
-    <div id={`todo-${id}`} class="flex flex-row space-x-3">
+    <div id={containerId} class="flex flex-row space-x-3">
       <p>{content}</p>
       <input
         type="checkbox"
         checked={completed}
         hx-post={`/todos/toggle/${id}`}
-        hx-target={`#todo-${id}`}
+        hx-target={`#${containerId}`}
         hx-swap="outerHTML"
       />
       <button
         class="text-red-500"
         hx-delete={`/todos/${id}`}
-        hx-target={`#todo-${id}`}
+        hx-target={`#${containerId}`}
         hx-swap="outerHTML"
       >
         X
