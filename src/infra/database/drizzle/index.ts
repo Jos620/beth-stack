ebb75765095c49b6cce19122ccd9af8e1e1394af
Mateus Ito <mateusito10@gmail.com>
2023-09-11 22:59:59 -0300

    feat(database): add TodosRepository implementation for Drizzle

---
+++ src/infra/database/drizzle/index.ts
@@ -0,0 +1,61 @@
+import { type Client, createClient } from '@libsql/client';
+import { eq } from 'drizzle-orm';
+import { drizzle, type LibSQLDatabase } from 'drizzle-orm/libsql';
+
+import { Todo } from '@/app/entities/todo';
+import type { TodosRepository } from '@/app/repositories/todos.repo';
+
+import * as schema from './schema';
+import { todos } from './schema';
+
+export class DrizzleRepository implements TodosRepository {
+  private readonly client: Client;
+  private readonly db: LibSQLDatabase<typeof schema>;
+
+  constructor() {
+    this.client = createClient({
+      url: process.env.DATABASE_URL!,
+      authToken: process.env.DATABASE_AUTH_TOKEN,
+    });
+
+    this.db = drizzle(this.client, {
+      schema,
+      logger: true,
+    });
+  }
+
+  async getTodo(id: number): Promise<Todo | null> {
+    return (
+      (await this.db.select().from(todos).where(eq(todos.id, id)).get()) || null
+    );
+  }
+
+  async getTodos(): Promise<Todo[]> {
+    return await this.db.select().from(todos).all();
+  }
+
+  async createTodo(params: { content: string }): Promise<Todo> {
+    const newTodo = await this.db
+      .insert(todos)
+      .values(params)
+      .returning()
+      .get();
+
+    return newTodo;
+  }
+
+  async updateTodo(id: number, overrides: Partial<Todo>): Promise<Todo> {
+    const newTodo = await this.db
+      .update(todos)
+      .set(overrides)
+      .where(eq(todos.id, id))
+      .returning()
+      .get();
+
+    return newTodo;
+  }
+
+  async deleteTodo(id: number): Promise<void> {
+    await this.db.delete(todos).where(eq(todos.id, id)).run();
+  }
+}
