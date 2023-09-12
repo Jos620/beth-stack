93eaf5eb1d53862dcf56ce662f56c5255b28c6c8
Mateus Ito <mateusito10@gmail.com>
2023-09-11 23:42:01 -0300

    feat(database): add DrizzleTodoMapper

---
+++ src/infra/database/drizzle/mappers/todos.mapper.ts
@@ -0,0 +1,27 @@
+import { InferSelectModel } from 'drizzle-orm';
+
+import { Todo } from '@/app/entities/todo';
+
+import { todos } from '../schema';
+
+export type DrizzleTodo = InferSelectModel<typeof todos>;
+
+export class DrizzleTodoMapper {
+  static toPersistance(todo: Todo): DrizzleTodo {
+    return {
+      id: todo.id,
+      content: todo.content,
+      completed: todo.completed,
+    };
+  }
+
+  static toDomain(todo: DrizzleTodo): Todo {
+    return new Todo(
+      {
+        content: todo.content,
+        completed: todo.completed,
+      },
+      todo.id,
+    );
+  }
+}
