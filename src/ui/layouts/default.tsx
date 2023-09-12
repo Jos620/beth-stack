2ade100943b3034c7f85f0398c64c0f4c870a07a
Mateus Ito <mateusito10@gmail.com>
2023-09-12 15:36:40 -0300

    feat(layouts): add hyperscript

--- src/ui/layouts/default.tsx
+++ src/ui/layouts/default.tsx
@@ -10,6 +10,7 @@ export const DefaultBaseHtml = ({ children }: elements.Children) => /* html */ `
     <title>BETH stack</title>
     <script src="https://unpkg.com/htmx.org@1.9.3"></script>
     <script src="https://unpkg.com/alpinejs@3" defer></script>
+    <script src="https://unpkg.com/hyperscript.org@0.9.11"></script>
     <script src="https://cdn.jsdelivr.net/npm/@unocss/runtime"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@unocss/reset/tailwind.css" />
   </head>
