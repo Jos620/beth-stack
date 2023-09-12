d7e917a429ac62d84f72995b196c3cd25ee6382d
Mateus Ito <mateusito10@gmail.com>
2023-09-11 23:42:29 -0300

    feat(database): use mapper to convert db entities to domain entities

--- src/infra/database/drizzle/index.ts
+++ src/infra/database/drizzle/index.ts
@@ -5,8 +5,9 @@ import { drizzle, type LibSQLDatabase } from 'drizzle-orm/libsql';
 import { Todo } from '@/app/entities/todo';
 import type { TodosRepository } from '@/app/repositories/todos.repo';
 
+import { DrizzleTodoMapper } from './mappers/todos.mapper';
 import * as schema from './schema';
-import { todos } from './schema';
+import { todos as todos_table } from './schema';
 
 export class DrizzleRepository implements TodosRepository {
   private readonly client: Client;
@@ -35,37 +36,50 @@ export class DrizzleRepository implements TodosRepository {
   }
 
   async getTodo(id: number): Promise<Todo | null> {
-    return (
-      (await this.db.select().from(todos).where(eq(todos.id, id)).get()) || null
-    );
+    const todo = await this.db
+      .select()
+      .from(todos_table)
+      .where(eq(todos_table.id, id))
+      .get();
+
+    if (!todo) {
+      return null;
+    }
+
+    return DrizzleTodoMapper.toDomain(todo);
   }
 
   async getTodos(): Promise<Todo[]> {
-    return await this.db.select().from(todos).all();
+    const todos = (await this.db.select().from(todos_table).all()) || [];
+
+    return todos.map(DrizzleTodoMapper.toDomain);
   }
 
   async createTodo(params: { content: string }): Promise<Todo> {
     const newTodo = await this.db
-      .insert(todos)
+      .insert(todos_table)
       .values(params)
       .returning()
       .get();
 
-    return newTodo;
+    return DrizzleTodoMapper.toDomain(newTodo);
   }
 
   async updateTodo(id: number, overrides: Partial<Todo>): Promise<Todo> {
     const newTodo = await this.db
-      .update(todos)
-      .set(overrides)
-      .where(eq(todos.id, id))
+      .update(todos_table)
+      .set({
+        completed: overrides.completed,
+        content: overrides.content,
+      })
+      .where(eq(todos_table.id, id))
       .returning()
       .get();
 
-    return newTodo;
+    return DrizzleTodoMapper.toDomain(newTodo);
   }
 
   async deleteTodo(id: number): Promise<void> {
-    await this.db.delete(todos).where(eq(todos.id, id)).run();
+    await this.db.delete(todos_table).where(eq(todos_table.id, id)).run();
   }
 }
