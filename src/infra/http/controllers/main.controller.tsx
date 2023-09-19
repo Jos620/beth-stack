99e74db786a80b0371115bcd705d7d7fb865c211
Mateus Ito <mateusito10@gmail.com>
2023-09-19 01:04:30 -0300

    chore(controllers): remove unused code

--- src/infra/http/controllers/main.controller.tsx
+++ src/infra/http/controllers/main.controller.tsx
@@ -2,16 +2,8 @@ import html from '@elysiajs/html';
 import Elysia from 'elysia';
 import elements from 'typed-html';
 
-import { getAllTodos } from '@/app/services/todos.service';
-import { DrizzleRepository } from '@/infra/database/drizzle';
 import { HomePage } from '@/ui/pages/home';
-
-const db = DrizzleRepository.getInstance();
 
 export const mainController = new Elysia()
   .use(html())
-  .get('/', async ({ html }) => {
-    const todos = await getAllTodos(db);
-
-    return html(<HomePage todos={todos}></HomePage>);
-  });
+  .get('/', async ({ html }) => html(<HomePage />));
