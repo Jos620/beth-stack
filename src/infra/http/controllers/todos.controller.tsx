c954a7d3bdb33f6613428359364186052258de94
Mateus Ito <mateusito10@gmail.com>
2023-10-02 10:29:39 -0300

    fix(controllers): update TodosService usage

--- src/infra/http/controllers/todos.controller.tsx
+++ src/infra/http/controllers/todos.controller.tsx
@@ -1,12 +1,6 @@
 import { Elysia } from 'elysia';
 
-import {
-  createTodo,
-  deleteTodo,
-  getAllTodos,
-  toggleTodo,
-  updateTodo,
-} from '@/app/services/todos.service';
+import { TodosService } from '@/app/services/todos.service';
 import { DrizzleRepository } from '@/infra/database/drizzle';
 import { TodoItem } from '@/ui/components/Todo/Item';
 import { EmptyFallback, TodoList } from '@/ui/components/Todo/List';
@@ -14,19 +8,20 @@ import { EmptyFallback, TodoList } from '@/ui/components/Todo/List';
 import { todosModels } from '../dto/todos';
 
 const db = DrizzleRepository.getInstance();
+const service = new TodosService(db);
 
 export const todosController = new Elysia({ prefix: '/todos' })
   .use(todosModels)
   .get('/', async () => {
-    const todos = await getAllTodos(db);
+    const todos = await service.getAllTodos();
 
     return <TodoList todos={todos} />;
   })
   .post(
     '/',
     async ({ body }) => {
-      await createTodo(db, body.content);
-      const todos = await getAllTodos(db);
+      await service.createTodo(body.content);
+      const todos = await service.getAllTodos();
 
       return <TodoList todos={todos} />;
     },
@@ -37,7 +32,7 @@ export const todosController = new Elysia({ prefix: '/todos' })
   .post(
     '/toggle/:id',
     async ({ params }) => {
-      const todo = await toggleTodo(db, params.id);
+      const todo = await service.toggleTodo(params.id);
 
       return <TodoItem todo={todo} />;
     },
@@ -48,9 +43,9 @@ export const todosController = new Elysia({ prefix: '/todos' })
   .delete(
     '/:id',
     async ({ params }) => {
-      await deleteTodo(db, params.id);
+      await service.deleteTodo(params.id);
 
-      const todos = await getAllTodos(db);
+      const todos = await service.getAllTodos();
       if (!todos.length) return <EmptyFallback />;
     },
     {
@@ -60,7 +55,7 @@ export const todosController = new Elysia({ prefix: '/todos' })
   .put(
     '/:id',
     async ({ params, body }) => {
-      const newTodo = await updateTodo(db, params.id, body);
+      const newTodo = await service.updateTodo(params.id, body);
 
       return <TodoItem todo={newTodo} />;
     },
