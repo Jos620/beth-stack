4a8499e3323359afd7a1bc151ba088e651df56c0
Mateus Ito <mateusito10@gmail.com>
2023-10-14 13:29:37 -0300

    refactor(pages): remove default layout from home page

--- src/infra/http/controllers/main.controller.tsx
+++ src/infra/http/controllers/main.controller.tsx
@@ -1,8 +1,15 @@
 import html from '@elysiajs/html';
 import Elysia from 'elysia';
 
+import { DefaultLayout } from '@/ui/layouts/default';
 import { HomePage } from '@/ui/pages/home';
 
 export const mainController = new Elysia()
   .use(html())
-  .get('/', async ({ html }) => html(<HomePage />));
+  .get('/', async ({ html }) =>
+    html(
+      <DefaultLayout>
+        <HomePage />
+      </DefaultLayout>,
+    ),
+  );
--- src/ui/pages/home.tsx
+++ src/ui/pages/home.tsx
@@ -1,15 +1,14 @@
 import { Loading } from '../components/Loading';
 import { TodoForm } from '../components/Todo/Form';
-import { DefaultLayout } from '../layouts/default';
 
 export function HomePage() {
   return (
-    <DefaultLayout>
+    <>
       <div hx-get="/todos" hx-trigger="load" hx-swap="outerHTML">
         <Loading />
       </div>
 
       <TodoForm />
-    </DefaultLayout>
+    </>
   );
 }
