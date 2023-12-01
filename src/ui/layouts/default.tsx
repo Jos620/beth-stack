488d3bf0313a96ece79424e0967afeeae11d6edb
Mateus Ito <mateusito10@gmail.com>
2023-12-01 07:13:53 -0300

    chore(layouts): remove base HTML from default layout

--- src/ui/layouts/default.tsx
+++ src/ui/layouts/default.tsx
@@ -1,16 +1,13 @@
 import { PropsWithChildren } from '@kitajs/html';
 
-import { DefaultBaseHtml } from '@/ui';
 import { Navigation } from '@/ui/components/Navigation';
 
 export function DefaultLayout({ children }: PropsWithChildren) {
   return (
-    <DefaultBaseHtml>
-      <div class="flex flex-col justify-center items-center w-screen h-screen">
-        {children}
+    <div class="flex flex-col justify-center items-center w-screen h-screen">
+      {children}
 
-        <Navigation />
-      </div>
-    </DefaultBaseHtml>
+      <Navigation />
+    </div>
   );
 }
