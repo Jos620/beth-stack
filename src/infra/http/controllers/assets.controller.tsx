3892a433a24a7093ae70c82beb695c5ac6840efb
Mateus Ito <mateusito10@gmail.com>
2023-12-04 08:51:25 -0300

    chore(infra): prefix assets controller

--- src/infra/http/controllers/assets.controller.tsx
+++ src/infra/http/controllers/assets.controller.tsx
@@ -1,5 +1,6 @@
 import Elysia from 'elysia';
 
-export const assetsController = new Elysia().get('/styles', async () =>
-  Bun.file('public/uno.css')
+export const assetsController = new Elysia({ prefix: 'assets' }).get(
+  '/styles',
+  async () => Bun.file('public/uno.css')
 );
